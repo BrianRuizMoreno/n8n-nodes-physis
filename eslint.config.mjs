346152c54acbedeb7cb1b47c19c04fdd6f66cc60
Brian Ruiz Moreno <brian.ruizmoreno2311@gmail.com>
2026-01-16 17:52:06 -0300

    fix: resolver conflicto en eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 import { config } from '@n8n/node-cli/eslint';
 
 export default config;
-=======
-import { config } from '@n8n/node-cli/eslint';
-
-export default config;
->>>>>>> b7319d8b76a654549e2abdeb482227fb7a5a3542
