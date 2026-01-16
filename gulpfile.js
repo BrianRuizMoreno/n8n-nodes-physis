f0d711e1e355bfab5032af0acc4860ed74dd5b7f
Brian Ruiz Moreno <brian.ruizmoreno2311@gmail.com>
2026-01-16 18:01:25 -0300

    fix errores duplicados

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { src, dest, parallel } = require('gulp');
 
 function copyNodeIcons() {
@@ -16,23 +15,4 @@ function copyMeta() {
 		.pipe(dest('dist'));
 }
 
-=======
-const { src, dest, parallel } = require('gulp');
-
-function copyNodeIcons() {
-	return src('nodes/**/*.{svg,png}')
-		.pipe(dest('dist/nodes'));
-}
-
-function copyCredentialIcons() {
-	return src('credentials/**/*.{svg,png}')
-		.pipe(dest('dist/credentials'));
-}
-
-function copyMeta() {
-	return src(['package.json', 'README.md', 'LICENSE'])
-		.pipe(dest('dist'));
-}
-
->>>>>>> b7319d8b76a654549e2abdeb482227fb7a5a3542
 exports.default = parallel(copyNodeIcons, copyCredentialIcons, copyMeta);
