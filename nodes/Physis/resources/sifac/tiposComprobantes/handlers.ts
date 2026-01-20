79750f93ca6eb7f357215b1dd909d0439b178a6f
Brian Ruiz Moreno <brian.ruizmoreno2311@gmail.com>
2026-01-20 14:28:04 -0300

    corrige tipocomprobantes

--- nodes/Physis/resources/sifac/tiposComprobantes/handlers.ts
+++ nodes/Physis/resources/sifac/tiposComprobantes/handlers.ts
@@ -7,7 +7,7 @@ export async function execute(this: IExecuteFunctions, index: number): Promise<I
 	
 	const baseUrlSifac = '/phy2service/api/sifac';
 	let endpoint = '';
-	const method = 'GET';
+	let method = 'GET';
 	let body: IDataObject = {};
 	let qs: IDataObject = {};
 
